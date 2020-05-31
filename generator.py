3aa2634eb7684584c337fe89205e7df229ce6915
Yunshen <578431642@qq.com>
2020-05-31 22:07:18 +0800

    modified:   generator.py 	modified:   train.py

--- generator.py
+++ generator.py
@@ -6,12 +6,13 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1,10,3,padding=1) #输入（batch_dize,1,28,28），卷积核尺寸（batch_size,3,3,3）
-        self.conv2 = nn.Conv2d(10,1,3,padding=1)
+        self.conv2 = nn.Conv2d(20,10,3,padding=1)
+        self.conv3 = nn.Conv2d(10,1,3,padding=1)
 
         self.relu = nn.LeakyReLU()
         self.tanh = nn.Tanh()
 
     def forward(self,x):
-        model = nn.Sequential(self.conv1,self.relu,self.conv2,self.tanh)
+        model = nn.Sequential(self.conv1,self.relu,self.conv2,self.relu,self.conv3,self.tanh)
         x = model(x)
         return x
