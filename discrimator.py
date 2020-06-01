d7ece0a2b64b0ae6ee904114ae724a8ee0065ada
Yunshen <578431642@qq.com>
2020-06-01 18:57:57 +0800

    77777

--- discrimator.py
+++ discrimator.py
@@ -5,19 +5,20 @@ class Discrimiator(nn.Module):
     def __init__(self):
         super().__init__()#细节 这没有冒号
 
-        self.fc1 = nn.Linear(28*28,10) #细节 这参数输入元素数，不用加batch batch输入的时候加
-        self.fc2 = nn.Linear(10,1)
+        self.fc1 = nn.Linear(28*28,500) #细节 这参数输入元素数，不用加batch batch输入的时候加
+        self.fc2 = nn.Linear(500,100)
+        self.fc3 = nn.Linear(100,10)
+        self.fc4 = nn.Linear(10,1)
         self.relu = nn.LeakyReLU()
 
         self.sigmod = nn.Sigmoid()
 
 
 
+
     def forward(self,x):
         in_size = x.size(0)
         x = x.view(in_size,-1)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.sigmod(x)
+        model = nn.Sequential(self.fc1,self.relu,self.fc2,self.relu,self.fc3,self.relu,self.fc4,self.sigmod)
+        x = model(x)
         return x
--- generator.py
+++ generator.py
@@ -5,15 +5,21 @@ import torch.nn as nn
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1,32,3,padding=1) #输入（batch_dize,1,28,28），卷积核尺寸（batch_size,3,3,3）
-        self.conv2 = nn.Conv2d(32,64,3,padding=1)
-        self.conv3 = nn.Conv2d(64,16,3,padding=1)
-        self.conv4 = nn.Conv2d(16,1,3,padding=1)
+        self.conv1 = nn.Conv2d(1,10,3,padding=1) #输入（batch_dize,1,28,28），卷积核尺寸（batch_size,3,3,3）
+        self.conv2 = nn.Conv2d(10,1,3,padding=1)
+        self.fc1 = nn.Linear(28*28,28*28)
+
+
 
         self.relu = nn.LeakyReLU()
         self.tanh = nn.Tanh()
 
     def forward(self,x):
-        model = nn.Sequential(self.conv1,self.relu,self.conv2,self.relu,self.conv3,self.relu,self.conv4,self.tanh)
+        in_size = x.size(0)
+        model = nn.Sequential(self.conv1,self.relu,self.conv2,self.relu)
         x = model(x)
+        x = x.view(in_size,-1)
+        x = self.fc1(x)
+        x = self.tanh(x)
+        x = x.view(in_size,1,28,28)
         return x
